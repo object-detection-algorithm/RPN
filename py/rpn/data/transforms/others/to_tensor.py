b14c6090aa79a7dfd96d21bc2a9f238ede284e23
zhujian <505169307@qq.com>
2020-05-23 18:46:18 +0800

    perf(transform): 调用PyTorch函数ToTensor

--- py/rpn/data/transforms/others/to_tensor.py
+++ py/rpn/data/transforms/others/to_tensor.py
@@ -8,9 +8,16 @@
 """
 
 import torch
+import torchvision.transforms as transforms
 import numpy as np
 
 
 class ToTensor(object):
+
     def __call__(self, cvimage, boxes=None, labels=None):
-        return torch.from_numpy(cvimage.astype(np.float32)).permute(2, 0, 1), boxes, labels
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ToTensor(),
+        ])
+
+        return transform(cvimage.astype(np.uint8)), boxes, labels
